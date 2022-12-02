4af2fe8f2fa88b1523467cac214312c2665baa9d
Ivanzmartins <ivanzmartins@gmail.com>
2022-12-01 22:35:40 -0300

    Corrigindo lint

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,13 +1,13 @@
 // const { User } = require('../models');
 
-const loginUser = async (email, password) => {
-  const user = await User.findOne({ where: { email } });
+// const loginUser = async (email, password) => {
+//   const user = await User.findOne({ where: { email } });
 
-  if (!user || user.password !== password) {
-    return { message: 'Invalid fields' };
-  }
-};
+//   if (!user || user.password !== password) {
+//     return { message: 'Invalid fields' };
+//   }
+// };
 
-module.exports = {
-  loginUser,
-};
+// module.exports = {
+//   loginUser,
+// };
